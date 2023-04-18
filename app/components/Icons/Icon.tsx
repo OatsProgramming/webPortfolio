b5ac9ed481f36e7c71661bb4b30c772fd6960d90
Jack Oats <jackoatmeals@gmail.com>
2023-04-17 21:29:22 -0500

    test to see which img loading is faster: statically imported and placed in a different file: 8.3 s on 3G; Hard coded faster: used hashmap to help

--- app/components/Icons/Icon.tsx
+++ app/components/Icons/Icon.tsx
@@ -1,21 +1,14 @@
-'use client'
-import Image, { StaticImageData } from 'next/image'
-import { useState } from 'react'
+import Image from 'next/image'
+import iconList from './iconList'
 
 export default function Icon({ size = 100, iconName, gray }: IconProps){
-    const [icon, setIcon] = useState('' as unknown as StaticImageData)
     // Icons that are hard to see in dark mode 
     // ( wont invert on its own unless svg is exported as component )
     const darkMode = ['nextjs', 'framer', 'swr']
 
-    // zustandIcon is a png
-    import(`@/public/techStack/${iconName}Icon.${iconName === 'zustand' ? 'png' : 'svg'}`)
-        .then(mod => setIcon(mod.default))
-        .catch(err => console.log(err))
-
     return (
         <Image 
-            src={icon} 
+            src={iconList[iconName]} 
             // If png, increase size by 2
             width={`${iconName === 'zustand' ? size * 2 : size }`} 
             alt={iconName} 
---
+++ app/components/Icons/iconList.ts
@@ -0,0 +1,26 @@
+import html from '@/public/techStack/htmlIcon.svg'
+import css from '@/public/techStack/cssIcon.svg'
+import js from '@/public/techStack/jsIcon.svg'
+import react from '@/public/techStack/reactIcon.svg'
+import nextjs from '@/public/techStack/nextjsIcon.svg'
+import zustand from '@/public/techStack/zustandIcon.png'
+import framer from '@/public/techStack/framerIcon.svg'
+import swr from '@/public/techStack/swrIcon.svg'
+import vite from '@/public/techStack/viteIcon.svg'
+import ts from '@/public/techStack/tsIcon.svg'
+
+// hashmap it
+const iconList = {
+    html,
+    css,
+    js,
+    react,
+    nextjs,
+    zustand,
+    framer,
+    swr,
+    vite,
+    ts,
+}
+
+export default iconList
